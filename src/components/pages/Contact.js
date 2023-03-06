f7d5870fcbbe1303c2a61e9fecc6069f1c1d4142
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2023-03-05 21:19:16 -0800

    portfolio updated

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,6 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Contact() {
   return (
-
     <Container>
       <Row>
         <Col>
@@ -12,7 +11,6 @@ export default function Contact() {
         </Col>
         <Col>
         <a href="https://www.linkedin.com/in/parkminjoo/" target="_blank"> <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Logo.svg.original.svg" class="contact"/></a> 
-        
         <a href="https://github.com/D0KD0/" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png" class="contact"/></a>
 
         </Col>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,7 +5,7 @@ import project3 from "../image/recipe.jpeg";
 import project4 from "../image/HelloBud_logo.png";
 import project5 from "../image/pre-work.jpeg";
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Figure } from 'react-bootstrap';
 
 export default function Portfolio() {
   return (
@@ -13,51 +13,73 @@ export default function Portfolio() {
     <Container>
       <Row>
         <Col>
+        <Figure>
           <a href="https://d0kd0.github.io/1st-Challenge-Horiseon/" target='_blank' >
-            <img
+            <Figure.Image
+              width={170}
+              height={170}
               src={project1}
               alt="desk at cafe"
               className="resize"
             />
           </a>
+        </Figure>
         </Col>
         <Col>
+        <Figure>
         <a href="https://d0kd0.github.io/2nd-Challenge-D0KD0/" target='_blank' >
-            <img 
-                src={project2}
-                alt="ipad on desk"
-                className="resize"
-              />
-            </a>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-            <a href="https://d0kd0.github.io/prework-study-guide/" target='_blank' >
-              <img 
-                src={project3}
-                alt="coding sign"
-                className="resize"
-              />
-            </a>
-        </Col>
-        <Col>
-        <a href="https://hellobud-app.herokuapp.com/" target='_black' >
-            <img 
-            src={project5}
-            alt="logo"
-            className="resize"
+            <Figure.Image
+              width={170}
+              height={170}
+              src={project2}
+              alt="desk at cafe"
+              className="resize"
             />
           </a>
+        </Figure>
+        </Col>
+      </Row>
+      
+      <Row>
+        <Col>
+        <Figure>
+        <a href="https://d0kd0.github.io/prework-study-guide/" target='_blank' >
+        <Figure.Image
+          width={170}
+          height={170}
+          src={project3}
+          alt="desk at cafe"
+          className="resize"
+        />
+        </a>
+        </Figure>
+        </Col>
+
+        <Col>
+        <Figure>
+          <a href="https://hellobud-app.herokuapp.com/" target='_black' >
+          <Figure.Image
+            width={170}
+            height={170}
+            src={project4}
+            alt="desk at cafe"
+            className="resize"
+          />
+          </a>
+        </Figure>
         </Col>
         <Col>
-        <a href="https://olgagav.github.io/what-am-i-craving-today" target='_black' >
-              <img 
-                src={project4}
-                alt="ipad on desk"
-                className="resize"
-              />
-            </a>
+        <Figure>
+          <a href="https://olgagav.github.io/what-am-i-craving-today" target='_black' >
+          <Figure.Image
+            width={170}
+            height={170}
+            src={project5}
+            alt="desk at cafe"
+            className="resize"
+          />
+          </a>
+        </Figure>
         </Col>
       </Row>
     </Container>
